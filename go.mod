f682ee9d509f96024070ae71530d6b97aa0980ad
Brayan Coba <cobabrayan@hotmail.com>
2023-10-03 16:46:10 -0500

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Brayan-Coba/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Brayan-Coba/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
